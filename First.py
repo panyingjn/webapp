889f72a20e66906733b86dbdc9c94e915f7664b0
panyingjn <40552033+panyingjn@users.noreply.github.com>
2018-07-07 16:45:33 +0800

    wrote a readme file

---
+++ First.py
@@ -0,0 +1,17 @@
+#！/usr/bin/env python3
+#-*-coding:utf-8-*-
+
+#y=ax*2+bx+c的解
+import math
+def quadratic(a,b,c):
+    if not isinstance(a,b,c, int):
+        raise TypeError('bad operand type')
+    if (b**2-4*a*c)<0:
+        print("no solution")
+    elif (b**2-4*a*c)==0:
+        x=-b/(2*a)
+        return x
+    else:
+        x=((-b+math.sqrt(b**2-4*a*c))/(2*a))
+        return x
+
