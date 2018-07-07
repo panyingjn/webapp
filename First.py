e494949095159e86b387301fe7bf691a02963889
panyingjn <40552033+panyingjn@users.noreply.github.com>
2018-07-07 17:06:13 +0800

    add x2

--- First.py
+++ First.py
@@ -12,6 +12,7 @@ def quadratic(a,b,c):
         x=-b/(2*a)
         return x
     else:
-        x=((-b+math.sqrt(b**2-4*a*c))/(2*a))
-        return x
+        x1 = ((-b+math.sqrt(b**2-4*a*c))/(2*a))
+        x2 = ((-b -math.sqrt(b ** 2 - 4 * a * c)) / (2 * a))
+        return x1,x2
 
